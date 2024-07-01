3f1e69ebee2c80478dd4a2f7ef660f77a5ff8546
AndyDAntuono <andrea.dantuono88@gmail.com>
2024-07-02 00:09:00 +0200

    [UPD] i can't do the bonus exercise

---
+++ bonus/js/script.js
@@ -0,0 +1,31 @@
+/*
+function sumArray(array) {
+    let total = 0;
+    for (let i = 0; i < array.length; i++) {
+        total += array[i];
+    }
+
+    for (let i = 0; i < 10; i++) {
+        let number = prompt("Inserisci un numero: ")
+    }
+
+    return total;
+}
+
+let result =
+*/
+
+
+const array = []
+
+for (let i = 0; i < 10; i++) {
+    let number = prompt("Inserisci un numero: ")
+    console.log
+
+    
+    sommma += parseInt(number[i])
+    array.push(somma)
+    console-log
+}
+
+console.log(array)
